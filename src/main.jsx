bdec3d0d1874899dc652774927ce00ffbc7dfde9
emorgan <erinannmorgan@yahoo.com>
2026-05-11 15:17:06 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PlateInventoryLookup from './PlateInventoryLookup'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PlateInventoryLookup />
+  </React.StrictMode>
+)
